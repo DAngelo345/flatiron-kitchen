f6e0fd1a2c8dffb5daf8486dc497b9002efcaae8
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-06-29 00:50:26 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,5 @@ class Recipe < ApplicationRecord
     validates :name, presence: true
     has_many :recipe_ingredients
     has_many :ingredients, through: :recipe_ingredients
+    # accepts_nested_attributes_for :ingredients
 end
